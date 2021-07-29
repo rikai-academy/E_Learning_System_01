12de26202cb8025e9a62ce13303a4be9b6e7ae9c
leduyvuong <leduyvuong99@gmail.com>
2021-07-29 16:12:15 +0700

    View Home Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  module ApplicationHelper
+    def full_title(page_title = '')
+      base_title = "E-Learning"
+      if page_title.empty?
+          base_title
+      else
+          page_title + " | " + base_title
+      end
+    end
+  end  
 end
