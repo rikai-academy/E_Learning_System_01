f950bfe0b30e3c4698bf1c809432d24371008f7f
leduyvuong <leduyvuong99@gmail.com>
2021-08-01 15:44:09 +0700

    basicSignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   scope "(:locale)", locale: /en|vi/ do
     root 'static_pages#home'
-    get '/cate', to: 'static_pages#categories'
+    get '/cate',          to: 'static_pages#categories'
   end
+  resources :users
 end
