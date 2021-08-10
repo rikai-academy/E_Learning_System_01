4ee219e167b3f5bd0520f538f69a30235011f032
leduyvuong <leduyvuong99@gmail.com>
2021-08-10 20:11:09 +0700

    login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    if logged_in?
+      redirect_to home_path
+    end
+  end
+  def create
+    @user = User.where("username = ? or email = ?", params[:session][:username], params[:session][:username]).first
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to home_path
+    else
+      flash.now[:danger] = t("errors.fail_login")
+      render :new
+    end
+  end
+  def destroy
+    log_out
+    redirect_to root_url
+  end 
+end
